2620a54cabe3378317dcaa97fe2d8b1f09a6eb8d
Константин <32998913+Wakan228@users.noreply.github.com>
2024-02-13 10:32:28 +0200

    add route

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,13 @@ Route::get('/', function () {
 })->name('main');
 Route::get('/aboutCompany', [App\Http\Controllers\HomeController::class, 'aboutCompany'])->name('aboutCompany');
 Route::get('/aboutWater', [App\Http\Controllers\HomeController::class, 'aboutWater'])->name('aboutWater');
+Route::get('/blog', [App\Http\Controllers\HomeController::class, 'blog'])->name('blog');
+Route::get('/contacts', [App\Http\Controllers\HomeController::class, 'contacts'])->name('contacts');
+Route::get('/delivery', [App\Http\Controllers\HomeController::class, 'delivery'])->name('delivery');
+Route::get('/stock', [App\Http\Controllers\HomeController::class, 'stock'])->name('stock');
+Route::get('/store', [App\Http\Controllers\HomeController::class, 'store'])->name('store');
+Route::get('/cart', [App\Http\Controllers\HomeController::class, 'cart'])->name('cart');
+Route::get('/login', [App\Http\Controllers\HomeController::class, 'login'])->name('login');
 // Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
